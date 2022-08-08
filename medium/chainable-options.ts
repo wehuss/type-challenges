322ae709a7d09b15256f87bd54a55c4267904fdb
wehuss <1245613916@qq.com>
2022-08-08 21:18:16 +0800

    chainable-options

---
+++ medium/chainable-options.ts
@@ -0,0 +1,23 @@
+type Chainable<T={}>={
+  option<K extends string,V>(key:K,val:V):Chainable<T & Record<K,V>>
+  get():T
+}
+
+declare const config: Chainable
+
+const result = config
+  .option('foo', 123)
+  .option('name', 'type-challenges')
+  .option('bar', { value: 'Hello World' })
+  .get()
+
+// 期望 result 的类型是：
+namespace chainableOptions{
+  interface Result {
+    foo: number
+    name: string
+    bar: {
+      value: string
+    }
+  }
+}
