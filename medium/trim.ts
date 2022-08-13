9e91a1cfb56f50cc72d0976547f36a4dabc9b244
wehuss <1245613916@qq.com>
2022-08-13 23:03:41 +0800

    test

--- medium/trim.ts
+++ medium/trim.ts
@@ -2,3 +2,7 @@ namespace trim{
   type Trim<T extends string>=T extends ` ${infer R} `?Trim<R>:T
   type trimed = Trim<'  Hello World  '> // expected to be 'Hello World'
 }
+
+const str='abc'
+
+str.split('')
